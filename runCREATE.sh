f7ac40ba62cb1c7a409c9de2c3c55709d5592c11
sandrinecharousset <62063474+sandrinecharousset@users.noreply.github.com>
2025-01-06 11:49:35 +0100

    Delete runCREATE.sh
    replaced by create.sh

--- runCREATE.sh
+++
@@ -1,6 +0,0 @@
-#/bin/bash
-
-echo -e "\n${print_green}Launching plan4res dataset creation for $DATASET - [$start_time]${no_color}"
-
-# run script to create plan4res input dataset (ZV_ZineValues.csv ...)
-source ${INCLUDE}/create.sh 
