b2c25924107d118daa29dc7ef04ac12368bd22cd
sandrinecharousset <62063474+sandrinecharousset@users.noreply.github.com>
2025-01-06 11:52:23 +0100

    Delete runPOSTTREAT.sh
    replaced by postreat.sh

--- runPOSTTREAT.sh
+++
@@ -1,6 +0,0 @@
-#/bin/bash
-
-# this script runs the posttreatment script on investment results
-echo -e "\n${print_green}Launching post-treatment for $DATASET - [$start_time]${no_color}"
-
-source ${INCLUDE}/postreat.sh
