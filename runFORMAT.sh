0e307f877ad9a4b673179d352d42f5f4d92608cc
sandrinecharousset <62063474+sandrinecharousset@users.noreply.github.com>
2025-01-06 11:51:51 +0100

    Delete runFORMAT.sh
    replaced by format.sh

--- runFORMAT.sh
+++
@@ -1,6 +0,0 @@
-#/bin/bash
-
-echo -e "\n${print_green}Launching plan4res dataset formatting for $DATASET - [$start_time]${no_color}"
-
-# run formatting script to create netcdf input files
-source ${INCLUDE}/format.sh
