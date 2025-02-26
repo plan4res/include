2cb5f2b8c72f67436bdcdaaca47fe9020438c2fd
sandrinecharousset <sandrine.charousset@edf.fr>
2025-02-26 16:28:04 +0000

    update format.h to make it possible to include BVs in nc4

--- format.sh
+++ format.sh
@@ -8,6 +8,13 @@ else
 	rm -rf ${INSTANCE}/nc4_${mode1}
 fi
 
+if [ "${mode1}" = "simul" ] || [ "${mode1}" = "invest" ]; then
+	if check_ssv_output "${mode1}"; then 
+		cp ${INSTANCE}/results_${mode1}$OUT/bellmanvalues.csv ${INSTANCE}/csv_${mode1}/
+	elif check_ssv_output "optim"; then 
+		cp ${INSTANCE}/results_optim$OUT/bellmanvalues.csv ${INSTANCE}/csv_${mode1}/
+	fi
+fi
 # run formatting script to create netcdf input files for running the cem
 echo -e "\n${print_blue}        - Create netcdf input files: ${no_color}${P4R_ENV} python -W ignore${PYTHONSCRIPTS_IN_P4R}/format.py ${CONFIG_IN_P4R}/settings_format_${mode1}.yml ${CONFIG_IN_P4R}/settingsCreateInputPlan4res_${mode2}.yml ${DATASET}"
 
