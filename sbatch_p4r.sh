d7825ad1de2d25e5f291291168bc817c0cb52ade
sandrinecharousset <62063474+sandrinecharousset@users.noreply.github.com>
2025-02-28 18:08:04 +0100

    Update sbatch_p4r.sh

--- sbatch_p4r.sh
+++ sbatch_p4r.sh
@@ -14,7 +14,7 @@ export SINGULARITYENV_LD_LIBRARY_PATH=\${LD_LIBRARY_PATH}
 export P4R_SINGULARITY_IMAGE_PRESERVE=1 # avoid update of the container
 P4R_CMD="srun --wckey=${WCKEY} --nodes=1 --ntasks=1 --ntasks-per-node=1 --cpus-per-task=1 --mpi=pmix -l"
 
-source /efs/software/plan4res/P4REDF/p4r-env/scripts/include/run_p4r.sh "${ARGS[@]}"
+source $P4R_DIR/p4r-env/scripts/include/run_p4r.sh "${ARGS[@]}"
 echo "Nb Node used : $SLURM_JOB_NUM_NODES"
 echo "Nb tasks used : $SLURM_NTASKS"
 echo "Nb CPUs used : $SLURM_CPUS_ON_NODE"
